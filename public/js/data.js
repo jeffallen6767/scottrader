ef7e876acaff727f4391c59dd98ad5090ab1834b
Jeffrey Allen <1063272+jeffallen6767@users.noreply.github.com>
2017-03-16 00:00:15 -0700

    compute dividend data

--- public/js/data.js
+++ public/js/data.js
@@ -1,3 +1,7 @@
+// avoid problems like: 0.01 + 0.06  //0.06999999999999999
+function addReal(a, b) {
+  return Math.round((a + b) * 1e12) / 1e12;
+}
 
 function parseCsv(text) {
   console.log('text');
@@ -11,28 +15,84 @@ function parseCsv(text) {
 }
 
 function indexData(data) {
-  var keys = data.shift(),
+
+  console.log("indexData", data);
+
+  var keys = data[0],
+    numKeys = keys.length,
+    numData = data.length,
     vals = [],
-    idx = {},
-    u,v,w,x,y,z;
+    idx = {"keys":{},"calc":{}},
+    x,y,z;
+
   // set-up index
-  for (x=0, y=keys.length; x<y; x++) {
-    idx[keys[x]] = [];
+  for (x=0; x<numKeys; x++) {
+    idx.keys[keys[x]] = x;
   }
-  // partition data and index it in reverse to get time-ordered
-  for (x=-1, y=data.length-1; x<y; y--) {
-    v = data[y];
-    vals.push(v);
-    for (w=0, z=v.length; w<z; w++) {
-      u = keys[w];
-      idx[u].push(v[w]);
+
+  // reverse data to get time-ordered
+  for (x=0, y=numData-1; x<y; y--) {
+    // data line
+    z = data[y];
+    if (z.length === numKeys) {
+      // save copy
+      vals.push(z);
     }
-    
   }
-  console.log('keys', keys);
-  console.log('vals', vals);
-  console.log('idx', idx);
 
-		
+  return createIndexes(keys, vals, idx);
+}
 
+function createIndexes(keys, vals, idx) {
+
+  console.log("indexByKeys", keys, vals, idx);
+
+  var numVals = vals.length,
+    idxKeys = idx.keys,
+    ACTION_IDX = idxKeys["ActionNameUS"],
+    SYMBOL_IDX = idxKeys["Symbol"],
+    AMOUNT_IDX = idxKeys["Amount"],
+
+    idxCalc = idx.calc,
+
+    dividendIdxs = [],
+    dividendSymbols = {}, 
+    dividendTotals = {}, 
+    dividendTotal = 0,
+
+    key, val, symbol, value,
+
+    x,y,z,
+
+    what;
+
+  // calc dividends
+  for (x=0; x<numVals; x++) {
+    val = vals[x];
+    if (val[ACTION_IDX] === "Dividend") {
+      dividendIdxs.push(x);
+      symbol = val[SYMBOL_IDX];
+      value = val[AMOUNT_IDX];
+      // init if necessary
+      dividendSymbols[symbol] = dividendSymbols[symbol] || {"idxs":[],"vals":[]};
+      dividendSymbols[symbol].idxs.push(x);
+      dividendSymbols[symbol].vals.push(value);
+      // init if necessary
+      dividendTotals[symbol] = dividendTotals[symbol] || 0;
+      dividendTotals[symbol] = addReal(dividendTotals[symbol], value);
+      dividendTotal = addReal(dividendTotal, value);
+    }
+  }
+  idxCalc["dividends"] = {
+    "idxs": dividendIdxs,
+    "symbols": dividendSymbols,
+    "totals": dividendTotals,
+    "total": dividendTotal
+  };
+
+  return {
+    "keys": keys,
+    "vals": vals,
+    "idx": idx
+  };
 }
--- public/js/display.js
+++ public/js/display.js
@@ -1,5 +1,17 @@
+var indexedData;
 function initDisplay(data) {
-	var indexedData = indexData(data);
+	// <button class="btn btn-lg upload-btn" type="button">Upload File</button>
+	$('button.upload-btn').replaceWith(
+		$('<button class="btn btn-lg upload-btn" type="button">calc</button>').click(function() {
+			show(data);
+		})
+	);
+	show(data);
+}
+
+function show(data) {
+	console.log("show", data);
+	indexedData = indexData(data);
 	console.log("indexedData");
 	console.log(indexedData);
 }
